283d6b6a5591ef4506fc0e952f46a2e8e0b313ee
MullerCedric <muller.cedric.96@gmail.com>
2018-12-11 15:53:14 +0100

    Add User Seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \jiri\User::create([
+            'name' => 'Cédric Müller',
+            'email' => 'muller.cedric.96@gmail.com',
+            'email_verified_at' => now(),
+            'remember_token' => str_random(10),
+            'is_admin' => true,
+            'is_active' => true,
+        ]);
+        factory(jiri\User::class, 4)->create();
+    }
+}
