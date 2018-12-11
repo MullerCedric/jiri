880fd5f0bd832c2f1fe10488ab9339d21c4d7da4
MullerCedric <muller.cedric.96@gmail.com>
2018-12-11 12:03:00 +0100

    Add fields to user migration and make them fillable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,9 +18,11 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role')->default('student');
+            $table->string('password')->default('$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm');
+            $table->boolean('is_admin')->default(false);
             $table->boolean('is_active')->default(true);
+            $table->string('avatar_url')->nullable();
+            $table->string('portfolio_url')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
