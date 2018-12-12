519835a078be758ecd4bb673d91f1e733f55d5c7
MullerCedric <muller.cedric.96@gmail.com>
2018-12-12 10:47:29 +0100

    Update existing seeder to have known dummy data

--- database/seeds/ExaminationUserTableSeeder.php
+++ database/seeds/ExaminationUserTableSeeder.php
@@ -11,9 +11,34 @@ class ExaminationUserTableSeeder extends Seeder
      */
     public function run()
     {
+        \jiri\ExaminationUser::create([
+            'examination_id' => 1,
+            'user_id' => 1,
+            'role' => 'examiner',
+        ]);
+        \jiri\ExaminationUser::create([
+            'examination_id' => 1,
+            'user_id' => 2,
+            'role' => 'examiner',
+        ]);
+        \jiri\ExaminationUser::create([
+            'examination_id' => 1,
+            'user_id' => 3,
+            'role' => 'student',
+        ]);
+        \jiri\ExaminationUser::create([
+            'examination_id' => 1,
+            'user_id' => 4,
+            'role' => 'student',
+        ]);
+        \jiri\ExaminationUser::create([
+            'examination_id' => 1,
+            'user_id' => 5,
+            'role' => 'student',
+        ]);
         for ($i = 1; $i < 5; $i++){
             \jiri\ExaminationUser::create([
-                'examination_id' => 1,
+                'examination_id' => 2,
                 'user_id' => $i,
                 'role' => (bool)rand(0, 1) ? 'examiner' : 'student',
             ]);
--- database/seeds/ExaminationsTableSeeder.php
+++ database/seeds/ExaminationsTableSeeder.php
@@ -11,6 +11,12 @@ class ExaminationsTableSeeder extends Seeder
      */
     public function run()
     {
+        \jiri\Examination::create([
+            'name' => 'Jury janvier 2019',
+            'manager_id' => 1,
+            'start_time' => '2019-01-24 08:35:00',
+            'is_draft' => 0,
+        ]);
         factory(jiri\Examination::class, 4)->create();
     }
 }
